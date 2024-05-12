1fa8c0d5384e914eaacc8063ce9dad9d4d14d79d
Andre <andrevitorlopes@gmail.com>
2024-05-12 14:55:58 +0200

    Give SNS permissions to invoke lambda

--- iac/convert_video_lambda.tf
+++ iac/convert_video_lambda.tf
@@ -26,3 +26,11 @@ resource "aws_sns_topic_subscription" "topic_subscription" {
   protocol  = "lambda"
   endpoint  = aws_lambda_function.convert_video.arn
 }
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.convert_video.arn
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.topic.arn
+}
