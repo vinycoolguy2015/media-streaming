062e1d0329624c886ced31aeb4b2cf6912f626d3
Andre <andrevitorlopes@gmail.com>
2025-02-03 21:56:46 +0100

    Fix event input

--- apps/create-mediaconvert-job/src/index.ts
+++ apps/create-mediaconvert-job/src/index.ts
@@ -3,7 +3,13 @@ import {
   CreateJobCommand,
   type CreateJobCommandOutput,
 } from '@aws-sdk/client-mediaconvert';
-import type { EventBridgeEvent, Handler, S3Event } from 'aws-lambda';
+import type {
+  EventBridgeEvent,
+  Handler,
+  S3Event,
+  S3EventRecord,
+  S3ObjectCreatedNotificationEventDetail,
+} from 'aws-lambda';
 import { dash, hls } from './output_groups.js';
 
 const mediaConvertRole = process.env.MEDIA_CONVERT_ROLE || '';
@@ -13,56 +19,57 @@ const s3BucketDestination = process.env.S3_BUCKET_DESTINATION || '';
 const mediaConverter = new MediaConvertClient({});
 
 export const handler: Handler<
-  EventBridgeEvent<'Object Created', S3Event>,
+  EventBridgeEvent<'Object Created', S3ObjectCreatedNotificationEventDetail>,
   void
 > = async (
-  event: EventBridgeEvent<'Object Created', S3Event>
+  event: EventBridgeEvent<
+    'Object Created',
+    S3ObjectCreatedNotificationEventDetail
+  >
 ): Promise<void> => {
-  const jobPromises: Promise<CreateJobCommandOutput>[] = [];
+  const bucketInput = event.detail.bucket;
+  const key = event.detail.object.key;
 
-  for (const record of event.detail.Records) {
-    const bucketInput = record.s3.bucket;
-    const key = record.s3.object.key;
+  const fileInput = `${bucketInput}${key}`;
 
-    const fileInput = `${bucketInput}${key}`;
-
-    const createJobCommand = new CreateJobCommand({
-      Role: mediaConvertRole,
-      Queue: streamingQueue,
-      AccelerationSettings: {
-        Mode: 'PREFERRED',
-      },
-      StatusUpdateInterval: 'SECONDS_60',
-      Settings: {
-        OutputGroups: [hls(s3BucketDestination), dash(s3BucketDestination)],
-        Inputs: [
-          {
-            AudioSelectors: {
-              'Audio Selector 1': {
-                Offset: 0,
-                DefaultSelection: 'DEFAULT',
-                ProgramSelection: 1,
-              },
+  const createJobCommand = new CreateJobCommand({
+    Role: mediaConvertRole,
+    Queue: streamingQueue,
+    AccelerationSettings: {
+      Mode: 'PREFERRED',
+    },
+    StatusUpdateInterval: 'SECONDS_60',
+    Settings: {
+      OutputGroups: [hls(s3BucketDestination), dash(s3BucketDestination)],
+      Inputs: [
+        {
+          AudioSelectors: {
+            'Audio Selector 1': {
+              Offset: 0,
+              DefaultSelection: 'DEFAULT',
+              ProgramSelection: 1,
             },
-            VideoSelector: {
-              ColorSpace: 'FOLLOW',
-              Rotate: 'DEGREE_0',
-              AlphaBehavior: 'DISCARD',
-            },
-            FilterEnable: 'AUTO',
-            PsiControl: 'USE_PSI',
-            FilterStrength: 0,
-            DeblockFilter: 'DISABLED',
-            DenoiseFilter: 'DISABLED',
-            TimecodeSource: 'ZEROBASED',
-            FileInput: fileInput,
           },
-        ],
-      },
-    });
+          VideoSelector: {
+            ColorSpace: 'FOLLOW',
+            Rotate: 'DEGREE_0',
+            AlphaBehavior: 'DISCARD',
+          },
+          FilterEnable: 'AUTO',
+          PsiControl: 'USE_PSI',
+          FilterStrength: 0,
+          DeblockFilter: 'DISABLED',
+          DenoiseFilter: 'DISABLED',
+          TimecodeSource: 'ZEROBASED',
+          FileInput: fileInput,
+        },
+      ],
+    },
+  });
 
-    jobPromises.push(mediaConverter.send(createJobCommand));
+  try {
+    await mediaConverter.send(createJobCommand);
+  } catch (error: any) {
+    console.error('Failed creating media converter job', error);
   }
-
-  await Promise.all(jobPromises);
 };
