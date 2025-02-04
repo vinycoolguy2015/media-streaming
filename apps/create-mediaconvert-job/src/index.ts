7004b6dfda04187db957a0e45cb5146b7b630645
Andre <andrevitorlopes@gmail.com>
2025-02-04 22:13:13 +0100

    Use presets for output groups

--- apps/create-mediaconvert-job/src/index.ts
+++ apps/create-mediaconvert-job/src/index.ts
@@ -37,31 +37,23 @@ export const handler: Handler<
   const createJobCommand = new CreateJobCommand({
     Role: mediaConvertRole,
     Queue: streamingQueue,
-    // AccelerationSettings: {
-    //   Mode: 'PREFERRED',
-    // },
+    AccelerationSettings: {
+      Mode: 'DISABLED',
+    },
     StatusUpdateInterval: 'SECONDS_60',
     Settings: {
       OutputGroups: [hls(s3BucketDestination), dash(s3BucketDestination)],
+      TimecodeConfig: {
+        Source: 'ZEROBASED',
+      },
+      FollowSource: 1,
       Inputs: [
         {
           AudioSelectors: {
             'Audio Selector 1': {
-              Offset: 0,
               DefaultSelection: 'DEFAULT',
-              ProgramSelection: 1,
             },
           },
-          VideoSelector: {
-            ColorSpace: 'FOLLOW',
-            Rotate: 'DEGREE_0',
-            AlphaBehavior: 'DISCARD',
-          },
-          FilterEnable: 'AUTO',
-          PsiControl: 'USE_PSI',
-          FilterStrength: 0,
-          DeblockFilter: 'DISABLED',
-          DenoiseFilter: 'DISABLED',
           TimecodeSource: 'ZEROBASED',
           FileInput: fileInput,
         },
