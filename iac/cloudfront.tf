11f206a165e9bdfd4432103ecceb1a07397b309e
Andre <andrevitorlopes@gmail.com>
2025-02-05 21:32:01 +0100

    Use regional domain name correctly

--- iac/cloudfront.tf
+++ iac/cloudfront.tf
@@ -12,7 +12,7 @@ resource "aws_cloudfront_origin_access_control" "oac" {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.bucket.bucket
+    domain_name              = aws_s3_bucket.bucket.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
     origin_id                = local.origin_id
   }
